4b55740539e56b2e8babd051bf9f1bf31245f47f
Andrés Quiroga <quid.andres@gmail.com>
2018-06-01 15:52:17 -0500

    Added building form

--- buildings/forms.py
+++ buildings/forms.py
@@ -18,3 +18,24 @@ class BuildingForm(forms.ModelForm):
             'phone_number',
             'initial_period',
         )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['city'].empty_label = 'Ciudad'
+
+        fields = [
+            'name',
+            'document_number',
+            'logo',
+            'city',
+            'address',
+            'email',
+            'mobile_phone',
+            'phone_number',
+            'initial_period',
+        ]
+
+        for field in fields:
+            label = self.fields[field].label
+            self.fields[field].label = ''
+            self.fields[field].widget.attrs['placeholder'] = label
