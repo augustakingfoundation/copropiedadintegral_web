43e2ff53b66ca4e5cc7524e6bb656a3cc414254a
Andrés Quiroga <quid.andres@gmail.com>
2018-06-19 17:47:11 -0500

    Changed gettex by gettex_lazy

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import PermissionsMixin
 from django.core.validators import MinLengthValidator
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 
 
 class UserManager(BaseUserManager):
