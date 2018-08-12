4b8067c76ada0b1b2fd20ad5fc3369f7cf24d593
Andrés Quiroga <quid.andres@gmail.com>
2018-08-11 19:25:17 -0500

    Added coefficient field and validation to accounting form.

--- accounting/forms.py
+++ accounting/forms.py
@@ -17,6 +17,12 @@ class AccountingForm(forms.ModelForm):
     """
     Form to create the accounting model of a condominium.
     """
+    coefficient = forms.DecimalField(
+        max_value=0,
+        min_value=100,
+        label=_('coeficiente'),
+    )
+
     rate = forms.ChoiceField(
         label=_('tarifa'),
         choices=RATE_CHOICES,
@@ -33,3 +39,17 @@ class AccountingForm(forms.ModelForm):
             'nit_dian',
             'local_nit_dian',
         )
+
+    def clean_coefficient(self):
+        # Validation to the coefficient field. It must
+        # be a value between 0 and 100.
+        value = self.cleaned_data['coefficient']
+
+        if value:
+            if value < 0 or value > 100:
+                raise forms.ValidationError(
+                    _('El coeficiente debe ser un valor '
+                      'entre 0 y 100')
+                )
+
+        return value
